ce3211a567f1b1c2028b2df7f011d7627e4bba57
Moaaz-i <famile.sherif@gmail.com>
2025-12-16 19:25:57 +0300

    first commit

--- app/api/auth/verifyToken/route.ts
+++ app/api/auth/verifyToken/route.ts
@@ -1,7 +1,6 @@
 import {NextRequest, NextResponse} from 'next/server'
 import {getServerSession} from 'next-auth'
 import {handler} from '../[...nextauth]/route'
-import '@/lib/next-auth'
 
 export async function GET(request: NextRequest) {
   try {
